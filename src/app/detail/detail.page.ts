f7ae2061afadf09b1124f577462a8a2bb4d82843
ElijahKeane <45961692+ElijahKeane@users.noreply.github.com>
2019-04-26 22:44:58 +0100

    added functionality to add details to the note

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { NotesService } from '../services/notes.service';
+import { Notes } from '../interface/note';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailPage implements OnInit {
 
-  constructor() { }
+  private note:Notes;
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private notesService: NotesService, private navCtrl: NavController) { 
+    this.note = {
+      id: '',
+      title: '',
+      Body: ''
+    };
   }
 
+  ngOnInit() {
+    let noteId = this.route.snapshot.paramMap.get('id');
+
+    if(this.notesService.loaded)
+    {
+      this.note = this.notesService.getNote(noteId)
+    } 
+    else {
+      this.notesService.load().then(() => {
+        this.note = this.notesService.getNote(noteId)
+      });
+    }
+  }
+
+  updateNote(){
+    this.notesService.save();
+  }
+
+  deleteNote(){
+    this.notesService.deleteNote(this.note);
+    this.navCtrl.navigateBack('/notes');
+  }
 }
