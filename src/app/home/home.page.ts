ee8d98d3cad68b05d1d8bfb816cb7b76d35ad05a
ElijahKeane <45961692+ElijahKeane@users.noreply.github.com>
2019-04-26 22:32:28 +0100

    add note function added to homepage.ts and homepage.html slightly updated to account for this

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController, NavController } from '@ionic/angular';
+import { NotesService } from 'src/app/services/notes.service';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,38 @@ import { AlertController, NavController } from '@ionic/angular';
 })
 export class HomePage implements OnInit{
 
-  constructor(private alertCtrl: AlertController, private naveCtrl: NavController){
+  constructor(private notesService: NotesService, private alertCtrl: AlertController, private naveCtrl: NavController){
 
   }
   ngOnInit(){
+    this.notesService.load();
+  }
+
+  addNote(){
+
+    this.alertCtrl.create({
+      header: 'New Note',
+      message: 'Enter the title for this note',
+      inputs: [
+        {
+          type: 'text',
+          name: 'title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save new note',
+          handler: (data) => {
+            this.notesService.createNote(data.title);
+          }
+        }
+      ]
+    }).then((alert) => {
+      alert.present();
+    });
 
   }
 }
